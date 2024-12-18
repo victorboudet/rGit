f5e3ce8f220cbad225899d3e4d5a71a5565b696f
Victor Boudet <victor.boudet@epitech.eu>
2024-12-18 18:22:55 +0100

    (ADD) Adding creation of the .rgit folder during the init part

--- src/commands/init.rs
+++ src/commands/init.rs
@@ -1,3 +1,23 @@
+use std::{fs, io};
+
+fn create_rgit_dir() {
+    match fs::create_dir(".rgit") {
+        Ok(_) => {
+            println!(".rgit directory created successfully.");
+        }
+        Err(err) if err.kind() == io::ErrorKind::AlreadyExists => {
+            println!("The .rgit directory already exists.");
+            fs::remove_dir_all(".rgit").expect("Failed to remove file");
+            create_rgit_dir();
+        }
+        Err(err) => {
+            println!("Failed to create .rgit directory: {:?}", err);
+            panic!();
+        }
+    }
+}
+
 pub fn run() {
-    todo!()
+    create_rgit_dir();
+    todo!();
 }
