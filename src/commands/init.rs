5f20cf51959c1734c96d9513430e7074a8e7ac1b
Victor Boudet <victor.boudet@epitech.eu>
2024-12-28 06:36:33 +0100

    (ADD) Creating subfunctions for the creation of files and directories

--- src/commands/init.rs
+++ src/commands/init.rs
@@ -1,17 +1,49 @@
 use std::{fs, io};
 
-fn create_rgit_dir() {
-    match fs::create_dir(".rgit") {
+fn force_create_dir(name: &str) -> Result<(), io::Error> {
+    match fs::create_dir(name) {
         Ok(_) => {
-            println!(".rgit directory created successfully.");
+            println!("{} directory created successfully.", name);
+            Ok(())
         }
         Err(err) if err.kind() == io::ErrorKind::AlreadyExists => {
-            println!("The .rgit directory already exists.");
-            fs::remove_dir_all(".rgit").expect("Failed to remove file");
-            create_rgit_dir();
+            println!("The {} directory already exists.", name);
+            fs::remove_dir_all(name).expect("Failed to remove file");
+            force_create_dir(name)
         }
         Err(err) => {
-            println!("Failed to create .rgit directory: {:?}", err);
+            println!("Failed to create {} directory: {:?}", name, err);
+            Err(err)
+        }
+    }
+}
+
+fn force_create_file(name: &str) -> Result<(), io::Error> {
+    match fs::File::create(name) {
+        Ok(_) => {
+            println!("{} file created successfully.", name);
+            Ok(())
+        }
+        Err(err) if err.kind() == io::ErrorKind::AlreadyExists => {
+            println!("The {} file already exists.", name);
+            fs::remove_file(name).expect("Failed to remove file");
+            force_create_file(name)
+        }
+        Err(err) => {
+            println!("Failed to create {} file: {:?}", name, err);
+            Err(err)
+        }
+    }
+}
+
+fn create_rgit_dir() {
+    match force_create_dir(".rgit") {
+        Ok(_) => {
+            force_create_dir(".rgit/objects").expect("Failed to create objects directory");
+            println!("rGit directory created successfully.");
+        }
+        Err(err) => {
+            println!("Failed to create rGit directory: {:?}", err);
             panic!();
         }
     }
